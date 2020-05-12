6e4c851e42298858a5f603659defbc1ca7e24aa7
Pol-S <plsbnjy@gmail.com>
2020-05-12 18:07:13 -0500

    Added starter spell option to character creation in the front end

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,82 @@
 
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+
+    <h1>Roll a new character</h1>
+    <div>
+      <form>
+      <h3>Name:</h3>
+      <input type = "text" v-model="newCharacterName" />
+      <br>
+      <h3>Level:</h3>
+      <label for="characterLevel">Your character level:</label>
+      <select id = "newCharacterClass" v-model="newCharacterLevel">
+        <option value = "1">1</option>
+      </select> 
+      <br>
+      <h3>Class:</h3>
+      <label for="classList">Pick a class:</label>
+      <select id = "newCharacterClass" v-model="newCharacterClass">
+        <option value = "1">Sorcerer</option>
+        <option value = "2">Warlock</option>
+        <option value = "3">Wizard</option>
+      </select>  
+      <br>
+      <h3>Speciality:</h3>
+      <label for="speciality">Pick a speciality:</label>
+      <select id = "speciality" v-model="newCharacterSpeciality">
+        <option value = "nuker">Nuker. Because if there are no explosions are you doing magic?</option>
+        <option value = "utility">Utility. Always useful, always practical.</option>
+        <option value = "crowd control">Crowd control. Let other do the damage, you control the field.</option>
+        <option value = "the face">The Face. Who needs to fight? You get your way by just talking.</option>
+        <option value = "none">None. Do what you want, you crazy diamond.</option>
+      </select>
+
+      <br>
+      <h3>Pick a starter spell:</h3>
+      <label for="spell">Your first cantrip?:</label>
+      <select id = "spell" v-model="newCharacterSpell">
+        <option value = "1">Eldritch Blast</option>
+        <option value = "2">Fire Bolt</option>
+        <option value = "3">Friends</option>
+        <option value = "4">Gust</option>
+        <option value = "5">Mage Hand</option>
+        <option value = "6">Message</option>
+        <option value = "7">Mold Earth</option>
+        <option value = "8">Toll the Death</option>
+
+      </select>
+      <br>
+      <button v-on:click="createCharacter()">Create Character</button>
+      </form>
+    </div>
+
+    <h1>All characters</h1>
+    <div v-for="character in characters">
+      <h2>Name: {{ character.name }}</h2>
+      <h3>Class: {{ character.character_class}}</h3>
+      <h3>Level: {{ character.level }}</h3>
+      <h4>Chosen Speciality: {{character.speciality}} </h4>
+
+      <button v-on:click="showCharacter(character)">More details and full spell book</button>
+      <div v-if="currentCharacter === character">
+        <p>Player: {{character.user}}</p>
+        Spell list: 
+        <div v-for="spell in character.spells">
+          <p>{{spell.spell}}</p>
+          <p>{{spell.description}}</p>
+        </div>
+        <p>How good at nuking? {{character.nuke_score}} out of 5.</p>
+        <p>How good at control? {{character.cc_score}} out of 5.</p>
+        <p>How good at general utility? {{character.utility_score}} out of 5.</p>
+        <p>How good at social magic? {{character.face_score}} out of 5.</p>
+      </div >
+
+  
+      <br>
+      <br>
+    </div>
+
   </div>
 </template>
 
@@ -10,13 +85,52 @@
 </style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!",
+      characters: [],
+      currentCharacter: {},
+      newCharacterName: "",
+      newCharacterLevel: "",
+      newCharacterClass: "",
+      newCharacterSpeciality: "",
+      newCharacterSpell: "",
+      message: "Mirror, mirror on the wall",
     };
   },
-  created: function() {},
-  methods: {},
+  created: function() {
+    axios.get("/api/characters").then(response => {
+      this.characters = response.data;
+    });
+  },
+  methods: {
+    createCharacter: function() {
+      var params = {
+        name: this.newCharacterName,
+        level: parseInt(this.newCharacterLevel),
+        character_class_id: parseInt(this.newCharacterClass),
+        speciality: this.newCharacterSpeciality,
+        new_spell_id: parseInt(this.newCharacterSpell),
+        user_id: 3,
+      };
+      axios.post("/api/characters", params).then(response => {
+        this.characters.push(response.data);
+        this.newCharacterName = "";
+        this.newCharacterLevel = "";
+        this.newCharacterClass = "";
+        this.newCharacterSpeciality = "";
+      });
+    },
+
+    showCharacter: function(character) {
+      if (this.currentCharacter === character) {
+        this.currentCharacter = {};
+      } else {
+        this.currentCharacter = character;
+      }
+    },
+  },
 };
 </script>
