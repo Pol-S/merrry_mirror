ee5648c994f7e0b64a038e4b1159ceb88ea346dc
Pol-S <plsbnjy@gmail.com>
2020-05-12 21:10:53 -0500

    FIXED AN INDENTATION ERROR DON'T JUDGE ME KAREN

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="app">
-    	<section id="header">
-				<header>
-					<h1>Mirror Mirror</h1>
-					<p>What will you be?</p>
-				</header>
-				<footer>
-					<a href="#banner" class="button style2 scrolly-middle">Let's see the shape of your magic.</a>
-				</footer>
-			</section>
+    <section id="header">
+      <header>
+        <h1>Mirror Mirror</h1>
+        <p>What will you be?</p>
+      </header>
+      <footer>
+        <a href="#banner" class="button style2 scrolly-middle">Let's see the shape of your magic.</a>
+      </footer>
+    </section>
 
     <router-view/>
     <section id="footer">
